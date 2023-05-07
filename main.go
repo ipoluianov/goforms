0502fdad1f30dfe26edf5640bf281a050b0a01e9
Ivan Poluianov <ivan.poluianov@gmail.com>
2023-05-07 20:08:42 +0200

    Update main.go

--- main.go
+++ main.go
@@ -7,5 +7,14 @@ import (
 func main() {
 	ui.InitUI()
 	form := ui.NewForm()
+	panelTop := form.Panel().AddPanelOnGrid(0, 0)
+	panelTop.AddTextBlockOnGrid(0, 0, "Label")
+	panelTop.AddVSpacerOnGrid(0, 1)
+	panelBottom := form.Panel().AddPanelOnGrid(0, 1)
+	panelBottom.AddHSpacerOnGrid(0, 0)
+	btnOK := panelBottom.AddButtonOnGrid(1, 0, "OK", nil)
+	btnOK.SetMinWidth(100)
+	btnCancel := panelBottom.AddButtonOnGrid(2, 0, "Cancel", nil)
+	btnCancel.SetMinWidth(100)
 	ui.StartMainForm(form)
 }
